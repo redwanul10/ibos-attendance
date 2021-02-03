8bf9410ea9065263147d028e93d139bfa643855d
Akij Admin <admin@AGCORP-WS171.akij.net>
2021-02-03 18:31:33 +0600

    api binded

--- src/screens/app/attendanceList/index.js
+++ src/screens/app/attendanceList/index.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native'
+import React, { useEffect,useState } from 'react';
+import { View, StyleSheet, FlatList} from 'react-native'
 import IHeader from '../../../common/components/IHeader';
+import { getGlobalData } from '../../../common/functions/localStorage';
 import ICalender from './components/ICalender';
 import ListCard from './components/listCard'
+import { getAttendanceList } from './helper';
 
 const AttendanceList = () => {
+
+    const [globalData, setGlobalData] = useState({})
+    const [attdList, setAttdList] = useState([])
+
+    useEffect(() => {
+        getGlobalData(setGlobalData)
+    }, [])
+
+    useEffect(() => {
+        if (globalData?.profileData?.userId) {
+            getAttendanceList(
+                globalData?.profileData?.userId,
+                new Date().getMonth() +1,
+                new Date().getFullYear(),
+                setAttdList
+            )
+        }
+
+    }, [globalData])
     return (
         <>
             <IHeader />
             <View style={style.container}>
                 <ICalender />
-                <ListCard/>
-                <ListCard/>
-                <ListCard/>
-                <ListCard/>
+
+                {attdList.map(item =>  <ListCard />)}
+
+                <FlatList
+                    data={attdList}
+                    // keyExtractor={ item => ite}
+                    renderItem={({item}) => <ListCard />}
+                />
+               
+                {/* <ListCard />
+                <ListCard />
+                <ListCard /> */}
 
 
-                
+
             </View>
-            
+
         </>
     );
 }
