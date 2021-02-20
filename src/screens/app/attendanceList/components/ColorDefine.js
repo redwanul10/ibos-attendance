73e4c207ce226aa8c9aee616686cb3241a0816f7
MdNaimKhan <naim1248@gmail.com>
2021-02-20 11:41:28 +0600

    portrait mode added

--- src/screens/app/attendanceList/components/ColorDefine.js
+++ src/screens/app/attendanceList/components/ColorDefine.js
@@ -8,56 +8,69 @@ function ColorDefine({ data }) {
     <>
       <View style={styles.def}>
 
-        <View style={styles.defStyle}>
+        <View style={{flexDirection:'row'}}>
+
+          <View style={styles.defStyle}>
             <Text>Present</Text>
-        </View>
-        <View style={[styles.defStyle,{backgroundColor:'#F75A5A'}]}>
+          </View>
+          <View style={[styles.defStyle, { backgroundColor: '#F75A5A' }]}>
             <Text>Absent</Text>
-        </View>
-        <View style={[styles.defStyle,{backgroundColor:'#F5A328'}]}>
+          </View>
+          <View style={[styles.defStyle, { backgroundColor: '#F5A328' }]}>
             <Text>Late</Text>
+          </View>
+
         </View>
-        <View style={[styles.defStyle,{backgroundColor:'#DCDCDC'}]}>
+        <View  style={{flexDirection:'row'}}>
+          <View style={[styles.defStyle, { backgroundColor: '#DCDCDC' }]}>
             <Text>Off Day</Text>
-        </View>
-        <View style={[styles.defStyle,{backgroundColor:'#6EC3F3'}]}>
+          </View>
+          <View style={[styles.defStyle, { backgroundColor: '#6EC3F3' }]}>
             <Text>Holiday</Text>
-        </View>
-        <View style={[styles.defStyle,{backgroundColor:'#AC88D5'}]}>
+          </View>
+          <View style={[styles.defStyle, { backgroundColor: '#AC88D5' }]}>
             <Text>Leave</Text>
+          </View>
+
         </View>
-        
-        
+
+
+
+
+
+
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-    def:{
-        
-        backgroundColor:'#ffffff',
-        // justifyContent:'space-around',
-        flexDirection:'row',
-        flexWrap:'wrap',
-        margin:10        
+  def: {
 
-    },
-    defStyle:{
-      backgroundColor:'#33D449',
-      
-      borderRadius:8,
-      paddingHorizontal:"5%",
-      paddingVertical:8,
-      alignItems:'center',
-      margin:3
-      
-      
+    backgroundColor: '#ffffff',
+    // justifyContent:'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 10
 
-    }
+  },
+  defStyle: {
+    backgroundColor: '#33D449',
+
+    borderRadius: 8,
+    // paddingHorizontal:"5%",
+     paddingVertical:8,
+    alignItems: 'center',
+    margin: 3,
+    width:"31.5%",
+    // height:"60%"
 
 
-  
+
+  }
+
+
+
 });
 
 export default ColorDefine;
