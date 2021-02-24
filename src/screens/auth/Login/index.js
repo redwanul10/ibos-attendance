02037ef65cdfa8af4105fbf11ba435f7b4c1f7a3
MdNaimKhan <naim1248@gmail.com>
2021-02-24 18:07:14 +0600

    redesigned login

--- src/screens/auth/Login/index.js
+++ src/screens/auth/Login/index.js
@@ -6,7 +6,8 @@ import FormInput from '../../../common/components/TextInput';
 import * as Yup from 'yup'
 import { loginAction } from './helper';
 import loginBgImg from '../../../assets/images/loginBg.png';
-import logo from '../../../assets/images/loginBanner.png';
+import logoBanner from '../../../assets/images/loginBanner.png';
+import logo from '../../../assets/images/loginLogo.png'
 import { getGlobalData } from '../../../common/functions/localStorage';
 
 
@@ -44,78 +45,94 @@ const Login = ({ navigation }) => {
     }, [])
 
     return (
+        <>
+            <View style={{ alignItems: "center" }}>
 
-        <View style={style.backgroundStyle}>
+                <Image style={{ width: "100%", height: 100, backgroundColor: "white" }} source={logoBanner} resizeMode="stretch" />
 
-            <TouchableWithoutFeedback onPress={e => Keyboard.dismiss()}>
+            </View>
 
-                <>
-                    <View style={{ marginBottom: "15%", alignItems: "center" }}>
-
-                        <Image style={{ width: "80%", height: 200 }} source={logo} resizeMode="stretch" />
-
-                    </View>
-
-                    <View >
-
-
-                        <Formik
-                            enableReinitialize={true}
-                            initialValues={{ ...initValues, email: globalData?.profileData?.emailAddress || "" }}
-                            validationSchema={schemaValidation}
-                            onSubmit={(values, { resetForm }) => {
-                                const { email, password } = values
-                                const customcb = () => {
-                                    resetForm()
-                                    
-                                    navigation.navigate("Attendance Dashboard")
-                                }
-                                loginAction(email, password, setIsLoading, customcb)
-                            }}
-                        >
-                            {(formikProps) => (
-                                <View>
-
-                                    <FormInput
-                                        name="email"
-                                        label="Email"
-                                        formikProps={formikProps}
-                                    />
-
-                                    <FormInput
-                                        name="password"
-                                        label="Password"
-                                        formikProps={formikProps}
-                                        secureTextEntry={true}
-                                    />
+            <View style={style.backgroundStyle}>
 
 
 
+                <TouchableWithoutFeedback onPress={e => Keyboard.dismiss()}>
 
-                                    <Button
-                                        block
-                                        onPress={e => {
-                                            formikProps.handleSubmit()
-                                        }}
-                                        style={style.btnStyle}>
+                    <>
+                        {/* <View style={{ marginBottom: "15%", alignItems: "stretch" }}>
 
-                                        <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>
-                                            Log In
+                            <Image style={{  height: 200,backgroundColor:"white" }} source={logo} resizeMode="stretch" />
+
+                        </View> */}
+                        <Image style={{ alignSelf: 'center', height: 100 }} source={logo} resizeMode="contain" />
+
+                        <View style={{alignItems:'center',margin:25}}>
+                            <Text style={{color:'#121E44',fontWeight:'bold',fontSize:20}}>Welcome to iBOS ERP!</Text>
+                            <Text style={{color:'#0C9EF2'}}>Sign in to continue</Text>
+
+                        </View>
+
+                        <View >
+
+
+                            <Formik
+                                enableReinitialize={true}
+                                initialValues={{ ...initValues, email: globalData?.profileData?.emailAddress || "" }}
+                                validationSchema={schemaValidation}
+                                onSubmit={(values, { resetForm }) => {
+                                    const { email, password } = values
+                                    const customcb = () => {
+                                        resetForm()
+
+                                        navigation.navigate("Attendance Dashboard")
+                                    }
+                                    loginAction(email, password, setIsLoading, customcb)
+                                }}
+                            >
+                                {(formikProps) => (
+                                    <View>
+
+                                        <FormInput
+                                            name="email"
+                                            label="Email"
+                                            formikProps={formikProps}
+                                        />
+
+                                        <FormInput
+                                            name="password"
+                                            label="Password"
+                                            formikProps={formikProps}
+                                            secureTextEntry={true}
+                                        />
+
+
+
+
+                                        <Button
+                                            block
+                                            onPress={e => {
+                                                formikProps.handleSubmit()
+                                            }}
+                                            style={style.btnStyle}>
+
+                                            <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>
+                                                Log In
                                         </Text>
 
-                                        {isLoading && <Spinner color='white' style={{ transform: [{ scaleX: 0.6 }, { scaleY: 0.6 }] }} />}
+                                            {isLoading && <Spinner color='white' style={{ transform: [{ scaleX: 0.6 }, { scaleY: 0.6 }] }} />}
 
 
-                                    </Button>
+                                        </Button>
 
-                                </View>
-                            )}
-                        </Formik>
-                    </View >
-                </>
-            </TouchableWithoutFeedback>
+                                    </View>
+                                )}
+                            </Formik>
+                        </View >
+                    </>
+                </TouchableWithoutFeedback>
 
-        </View>
+            </View>
+        </>
     );
 }
 
@@ -136,7 +153,7 @@ const style = StyleSheet.create({
         height: 166
     },
     btnStyle: {
-        backgroundColor: "#0080FF",
+        backgroundColor: "#121E44",
         marginTop: 20,
         height: 50,
         borderRadius: 50
